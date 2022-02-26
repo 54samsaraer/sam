2f6f6229f8f6eb8b223e5eae649c0e46753253f7
54samsaraer <99478661+54samsaraer@users.noreply.github.com>
2022-02-26 17:51:44 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,10 +341,10 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 99) return '犹豫就会败北！';
-    if (score <= 149) return '忍杀';
-    if (score <= 199) return '修';
-    return '罗！';
+    if (score <= 99) return '弱爆了！';
+    if (score <= 149) return '才这点分，自罚20遍！';
+    if (score <= 199) return '还远远达不到要求';
+    return '这有什么值得骄↓傲↑的？';
 }
 
 function toStr(obj) {
